1b6e4e985a8a7fc9e9b176a98592f61d807027f8
LavenderG <38311807+LavenderG@users.noreply.github.com>
2018-09-22 17:00:13 +0200

    Refactored new trainer type.
    Removed useless level table routine.

--- engine/battle/leader_level.asm
+++ engine/battle/leader_level.asm
@@ -1,40 +1,3 @@
-const_value = 0
-
-	const BROCK_LEVEL
-	const MISTY_LEVEL
-	const SURGE_LEVEL
-	const ERIKA_LEVEL
-	const KOGA_LEVEL
-	const SABRINA_LEVEL
-	const BLAINE_LEVEL
-	const GIOVANNI_LEVEL
-
-; Gets a Gym Leader minimum level
-; Parameters:
-; c: the gym leader
-; Returns: the minimum level in a
-GetLeaderMinimumLevel:
-	push bc
-	push hl
-
-	; get the level from the LeaderMinLevels table
-	ld b, 0
-	ld hl, .LeaderMinLevels
-	add hl, bc
-	ld a, [hl]
-
-	pop hl
-	pop bc
-	ret
-.LeaderMinLevels:
-	db 22 ; BROCK_LEVEL
-	db 20 ; MISTY_LEVEL
-	db 25 ; SURGE_LEVEL
-	db 35 ; ERIKA_LEVEL
-	db 40 ; KOGA_LEVEL
-	db 45 ; SABRINA_LEVEL
-	db 50 ; BLAINE_LEVEL
-	db 55 ; GIOVANNI_LEVEL
 
 ; Gets player's party max level
 ; Returns: the player's plarty max level in a
@@ -74,16 +37,14 @@ GetPlayerPartyMaxLevel:
 ; Gets a given leader level based on its minimum level
 ; and the player's party max level
 ; Parameters:
-; c: the gym leader
+; c: the gym leader minimum level
 ; Returns: the leader level in a
 GetLeaderLevel:
 	push bc
-	call GetLeaderMinimumLevel
-	ld b, a
 	call GetPlayerPartyMaxLevel
-	cp b
+	cp c
 	jr nc, .not_use_min_level
-	ld a, b
+	ld a, c
 .not_use_min_level
 	pop bc
 	ret
